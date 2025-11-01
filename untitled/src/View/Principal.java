b2f09fd4db28a427e981d98201668f9810d07991
João Vitor <jvtreloso@gmail.com>
2025-10-31 23:41:07 -0300

    Nome dos integrantes do grupo

--- untitled/src/View/Principal.java
+++ untitled/src/View/Principal.java
@@ -3,6 +3,12 @@ package View;
 import Cliente.Cliente;
 import Cliente.ClienteNovo;
 import Cliente.ClienteFidelidade;
+import Funcionarios.Funcionario;
+import Funcionarios.Caixa;
+import Funcionarios.Estoquista;
+import Funcionarios.Vendedor;
+import Funcionarios.Gerente;
+
 
 public class Principal {
     public static void main(String[] args) {
@@ -10,6 +16,8 @@ public class Principal {
         ClienteNovo novo = new ClienteNovo("13213144141","João Vitor","564862987-10","joaovitor@gmail.com","(81)-994542215\n");
         Cliente cliente = new Cliente("123414512DSAD","Marcelo","712343568-70","marcelinhotrevoso@gmail.com","(81)-95412376\n");
         ClienteFidelidade c1 = new ClienteFidelidade("AS13913","Joana","583423131-12","joanadarc@gmail.com","(80)-12353218\n");
+
+
         c1.setNome("Marcelo");
         c1.desconto(Double.parseDouble("30.20"));
 
@@ -18,5 +26,8 @@ public class Principal {
         cliente.cadastrar();
         novo.modificarCadastro("12390814","Marceline","574312839856","marcelinevampira@gmail.com","(18)-995627758\n");
         c1.avaliacao(4);
+
+
+
         }
     }
