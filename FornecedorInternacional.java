97042040e41209c1b94c6c3deb2ce36aa2f9ca5d
IuriAllexander <iuriallexander17@gmail.com>
2025-11-02 12:18:52 -0300

    Add files via upload

---
+++ FornecedorInternacional.java
@@ -0,0 +1,47 @@
+package fornecedor;
+
+public class FornecedorInternacional extends Fornecedor {
+
+    public FornecedorInternacional(String id, String nome, String cnpj, String email, String telefone, String endereco,
+    String tipoProdutoFornecido) {
+          super(id, nome, cnpj, email, telefone, endereco, tipoProdutoFornecido);
+    }
+
+    public double calcularTaxaImportacao(double valorCompra) {
+        double taxaImportacao = 0.25; // 25% de taxa alfandegária
+        return valorCompra * taxaImportacao;
+    }
+
+    public boolean exigeDocumentacaoExtra(String paisOrigem) {
+        return !paisOrigem.equalsIgnoreCase("Brasil") &&
+               !paisOrigem.equalsIgnoreCase("Argentina") &&
+               !paisOrigem.equalsIgnoreCase("Paraguai") &&
+               !paisOrigem.equalsIgnoreCase("Uruguai");
+    }
+
+    public String gerarResumoFornecedor() {
+        return "Fornecedor Internacional: " + getNome() + "\n" +
+               "CNPJ: " + getCnpj() + "\n" +
+               "Contato: " + getEmail() + " / " + getTelefone() + "\n" +
+               "Endereço: " + getEndereco() + "\n" +
+               "Produtos: " + getTipoProdutoFornecido();
+    }
+
+    public void enviarPedidoInternacional(String produto, int quantidade, int diasEntrega) {
+        System.out.println("Pedido internacional enviado para " + getNome() + ": " +
+                           quantidade + " unidades de " + produto +
+                           ". Prazo estimado: " + diasEntrega + " dias.");
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Tipo: Internacional");
+    }
+
+    @Override
+    public void cadastrar() {
+        super.cadastrar();
+        System.out.println("Fornecedor internacional cadastrado. Taxa de importação estimada: " + calcularTaxaImportacao(1000.0));
+    }
+}
---
+++ FornecedorNacional.java
@@ -0,0 +1,43 @@
+package fornecedor;
+
+public class FornecedorNacional extends Fornecedor {
+
+    public FornecedorNacional(String id, String nome, String cnpj, String email, String telefone, String endereco,
+    String tipoProdutoFornecido) {
+        super(id, nome, cnpj, email, telefone, endereco, tipoProdutoFornecido);
+    }
+
+    public boolean validarCNPJ() {
+        return getCnpj() != null && getCnpj().length() == 18;
+    }
+
+    public double calcularImpostoNacional(double valorCompra) {
+        double taxaImposto = 0.12; // 12% de imposto nacional
+        return valorCompra * taxaImposto;
+    }
+
+    
+    public String gerarResumoFornecedor() {
+        return "Fornecedor Nacional: " + getNome() + "\n" +
+               "CNPJ: " + getCnpj() + "\n" +
+               "Contato: " + getEmail() + " / " + getTelefone() + "\n" +
+               "Produtos: " + getTipoProdutoFornecido();
+    }
+
+    public void enviarPedido(String produto, int quantidade) {
+        System.out.println("Pedido enviado para " + getNome() + ": " + quantidade + " unidades de " + produto);
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Tipo: Nacional");
+        System.out.println("CNPJ Válido: " + (validarCNPJ() ? "Sim" : "Não"));
+    }
+
+    @Override
+    public void cadastrar() {
+        super.cadastrar();
+        System.out.println("Fornecedor nacional cadastrado. Imposto estimado: " + calcularImpostoNacional(1000.0));
+    }
+}
